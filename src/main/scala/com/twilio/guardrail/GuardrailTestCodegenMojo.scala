ad88628020bb97e6f9b75107fcb1487be2fa70f2
Devon Stewart <dstewart@tubi.tv>
2021-10-21 06:34:41 -0700

    Bumping

--- src/main/scala/com/twilio/guardrail/GuardrailTestCodegenMojo.scala
+++ src/main/scala/com/twilio/guardrail/GuardrailTestCodegenMojo.scala
@@ -3,6 +3,8 @@ package com.twilio.guardrail
 import java.io.File
 import org.apache.maven.plugins.annotations.{LifecyclePhase, Mojo, Parameter}
 
+import dev.guardrail.cli.{CLI, CLICommon}
+
 @Mojo(name = "generate-test-sources", defaultPhase = LifecyclePhase.GENERATE_TEST_SOURCES)
 class GuardrailTestCodegenMojo extends AbstractGuardrailCodegenMojo(Test) {
   @Parameter(defaultValue = "${project.build.directory}/generated-sources/swagger-test-clients", property = "outputPath", required = true)
