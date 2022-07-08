365c4d2b6c8d791e53fdba77302bbf5148ee2c45
Devon Stewart <blast@hardchee.se>
2022-07-07 23:08:33 -0700

    Upgrading guardrail

--- src/main/scala/dev/guardrail/GuardrailCodegenMojo.scala
+++ src/main/scala/dev/guardrail/GuardrailCodegenMojo.scala
@@ -3,12 +3,8 @@ package dev.guardrail
 import java.io.File
 import org.apache.maven.plugins.annotations.{LifecyclePhase, Mojo, Parameter}
 
-import dev.guardrail.generators.GeneratorMappings
-
 @Mojo(name = "generate-sources", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 class GuardrailCodegenMojo extends AbstractGuardrailCodegenMojo(Main) {
-  override def languages = GeneratorMappings.defaultLanguages
-
   @Parameter(defaultValue = "${project.build.directory}/generated-sources/swagger-clients", property = "outputPath", required = true)
   var outputPath: File = _
 }
